a53322e513ec2e4cfa2f1b5fdf80331b3180eb0c
qbgXyn <qbgXyn@gmail.com>
2021-11-15 20:03:50 +0800

    edit map.h

--- Map.h
+++ Map.h
@@ -6,7 +6,6 @@ class Map {
         static const int grid_radius = 8;
         double width; // x
         double height; // y
-        vector<Handle> processingList; // handle wait for processing in next tick e.g. handle being attacked
         vector<Handle> List; // all handle
     public:
         Map(double width, double height);
