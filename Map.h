0f809ae18fad8d069503e68b502b11fef75a012c
qbgXyn <qbgXyn@gmail.com>
2021-11-18 14:43:16 +0800

    resolve merge conflict

--- Map.h
+++ Map.h
@@ -9,7 +9,6 @@ class Map {
         vector<Handle> List; // all handle
     public:
         Map(double width, double height);
-<<<<<<< HEAD
         inline double Map::getMaxWidth() const {return width / 2;}
         inline double Map::getMaxHeight() const {return height / 2;}
         inline bool isCoordinateInMap(double x, double y) {return (x > 0) && (x <= width) && (y > 0) && (y <= height);}
@@ -22,8 +21,3 @@ class Map {
         bool hasCollision(const Handle &h, double x, double y);
 
 };
-=======
-        double getMaxWidth() const;
-        double getMaxHeight() const;
-};
->>>>>>> 4d04435af4cbfc175bc4205f5fd82de57786559e
