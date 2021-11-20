7c527eddd0e86613202013851a117e8352ab7d67
Jack143b <94368009+Jack143b@users.noreply.github.com>
2021-11-20 15:02:04 +0800

    Working on attack()

--- Handle/Unit/Survivor.cpp
+++ Handle/Unit/Survivor.cpp
@@ -1,5 +1,5 @@
 #include "Survivor.h"
-
+#include <chrono>
 const float Survivor::base_collison_radius = 16.0;
 const double Survivor::base_max_speed = 20.0;
 const float Survivor::base_attackInterval = 1.0;
@@ -12,9 +12,10 @@ Survivor::Survivor(Map* map, double x, double y) : Unit(map, x, y) {
 }
 
 void Survivor::attack() {
-    /*
-    set attack interval of survivor
-    get all Handle that within radius (60 degree of the circle(radius = attack_radius, center = Coordinate(Survivor)) in front of Survivor)
+    //set attack interval of survivor
+
+     auto t_start = std::chrono::high_resolution_clock::now();
+    /*get all Handle that within radius (60 degree of the circle(radius = attack_radius, center = Coordinate(Survivor)) in front of Survivor)
     if :
         1. Handle is not invulnerable
         2. Handle is Unit
@@ -23,4 +24,13 @@ void Survivor::attack() {
     deal (attack(Survivor) - armor(Unit)) amount of attack to Unit
 
     */
+    while (true)
+    {
+    auto t_end = std::chrono::high_resolution_clock::now();
+    if (std::chrono::duration<float, milli>(t_end - t_start).count() > 1 / base_attackInterval)
+    {
+        //cout << chrono::duration<float, milli>(t_end - t_start).count() << endl;
+        break;
+    }
+    }
 }
