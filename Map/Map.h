9984ab6315c4a400aea4447d575b991437a82333
Jack143b <94368009+Jack143b@users.noreply.github.com>
2021-11-28 20:11:45 +0800

    map comment

--- Map/Map.h
+++ Map/Map.h
@@ -1,5 +1,5 @@
 //This is the game map part. It contains basic setting like width and height with some helping functions like distanceBetweenPoint
-//and handle related functions.
+//It also contains function used in many cpp like handle, item and unit
 #ifndef MAP_H
 #define MAP_H
 
@@ -33,31 +33,37 @@ class Map {
     public:
         Map(double width, double height); //constructor
         ~Map(); //destructor
-        double getMaxWidth() const;
+
+        double getMaxWidth() const; //accessor 
         double getMaxHeight() const;
-        bool isCoordinateInMap(double x, double y) const;
-        Terrain::Type getTerrainOfGrid(double x, double y) const;
 
-        double getRandomLocation(double minValue, double maxValue) const;
+        bool isCoordinateInMap(double x, double y) const; //check if the coordinate exist
 
+        Terrain::Type getTerrainOfGrid(double x, double y) const; //convert x and y coordinate into corresponding grid
 
-        double distanceBetweenPoints(double x1, double y1, double x2, double y2) const;
+        double getRandomLocation(double minValue, double maxValue) const; //create random location
 
-        vector<Handle*> getHandleGroup(double x, double y, double radius);
+        double distanceBetweenPoints(double x1, double y1, double x2, double y2) const; //function to calculate distance between two points
 
-        Terrain::Type get_at(double x, double y) const;
-        static Item* createItem(Item::ID);
+        vector<Handle*> getHandleGroup(double x, double y, double radius); //a frequently called function which check all handles that exist within the circle with input radius
+
+        Terrain::Type get_at(double x, double y) const; //return the type of terrain of given location
+
+        static Item* createItem(Item::ID); //item related functions
         bool createItem_Handle(Item::ID id, double x, double y);
 
-        Handle* createHandle(Handle::Type type, double x, double y);
+        Handle* createHandle(Handle::Type type, double x, double y); //create and remove handle in handle list
         void removeHandle(Handle *h);
 
-        int getGameTime() const;
+        int getGameTime() const; //accessor and mutator of game time
         void setGameTime(int time);
        
-        Survivor* player;
-        vector<Handle*> List; // all handle
-        Terrain::Type** grid;
+        Survivor* player; //construct player
+
+        vector<Handle*> List; // vector conatiner of all handle 
+
+        Terrain::Type** grid; //map gird which store terrain type in every grid
+
         static const int grid_size = 64; // length of a square of a grid on the map, for displaying texture only(grass, sea, etc.)
 };
 #endif // MAP_H
