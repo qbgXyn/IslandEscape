21a0f9a436ede366eabb5a31291c97a6051fa69f
Jack143b <94368009+Jack143b@users.noreply.github.com>
2021-11-28 20:15:44 +0800

    terrain comment

--- Map/Terrain.cpp
+++ Map/Terrain.cpp
@@ -1,10 +1,10 @@
 #include "Terrain.h"
 
 
-bool Terrain::operator()(Terrain::Type terrain) const {
+bool Terrain::operator()(Terrain::Type terrain) const { //count number of terrain, return true if there has at least one terrain 
     return t.count(terrain);
 }
 
-void Terrain::operator+=(Terrain::Type terrain) {
+void Terrain::operator+=(Terrain::Type terrain) { //insert terrain into the container
     t.insert(terrain);
 }
