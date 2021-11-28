75b4b7170e9f235b4681504396efa4eb40293c56
Jack143b <94368009+Jack143b@users.noreply.github.com>
2021-11-28 15:53:09 +0800

    comment on unit

--- Handle/Unit/Unit.cpp
+++ Handle/Unit/Unit.cpp
@@ -2,29 +2,29 @@
 #include <chrono>
 #include "../../Map/Map.h"
 
-Unit::Unit(Map *map, double x, double y) : Handle(map, x, y) {
+Unit::Unit(Map *map, double x, double y) : Handle(map, x, y) { //constructor and pass value to handle constructor
     category = Handle::Category::UNIT;
 }
 
 
-float Unit::getDamage() const {
+float Unit::getDamage() const { //accessor
     return damage;
 }
 
-void Unit::setDamage(int damage) {
+void Unit::setDamage(int damage) { //mutator
     this->damage = damage;
 }
 
-float Unit::getArmor() const {
+float Unit::getArmor() const { //accessor
     return armor;
 }
 
 
-void Unit::setVisibleSize(int visible_size) {
+void Unit::setVisibleSize(int visible_size) { //mutator
     this->visible_size = visible_size;
 }
 
-void Unit::gainAttributeFromEffect(Effect *e) {
+void Unit::gainAttributeFromEffect(Effect *e) { //gain effect
     Effect::Type type = e->getType();
     switch(type) {
         case Effect::Type::REGEN_INSTANT:
@@ -42,19 +42,19 @@ void Unit::gainAttributeFromEffect(Effect *e) {
     }
 }
 
-void Unit::addEffect(Effect *e) {
+void Unit::addEffect(Effect *e) { //add effect into the vector list
     gainAttributeFromEffect(e);
     EffectList.push_back(e);
 }
 
-bool Unit::isInsideSector(const Handle *h, double sector_angle) const
+bool Unit::isInsideSector(const Handle *h, double sector_angle) const //check if the target is within the circular sector
 {
     double half_angle = sector_angle / 2;
     double current_direction = this -> getDirection();
-    double start_angle = current_direction - half_angle;
-    double end_angle = current_direction + half_angle;
+    double start_angle = current_direction - half_angle; //the start angle of the radius
+    double end_angle = current_direction + half_angle; //end angle of the radius
     double target_handle_angle = atan(h -> getY() / h -> getX());
-    if (target_handle_angle >= start_angle && target_handle_angle <= end_angle)
+    if (target_handle_angle >= start_angle && target_handle_angle <= end_angle) //return true if it is within the radius sector
     {
         return true;
     }
@@ -62,25 +62,29 @@ bool Unit::isInsideSector(const Handle *h, double sector_angle) const
 }
 
 
-int Unit::getVisibleSize() const {
+int Unit::getVisibleSize() const { //accessor
     return visible_size;
 }
-bool Unit::isGridVisible(int x, int y) const {
+
+bool Unit::isGridVisible(int x, int y) const { //check if the grid is visible as the player will move around. Keep calling this function to check the visible grids
     int u_x = ( static_cast<int> (location[0]) ) / map->grid_size;
     int u_y = ( static_cast<int> (location[1]) ) / map->grid_size;
 
     return ( abs(u_x - x) <= visible_size ) && ( abs(u_y - y) <= visible_size );
 }
 
-bool Unit::isHandleVisible(Handle *h) const {
+bool Unit::isHandleVisible(Handle *h) const { //check if the handle is visible for player
     return isGridVisible(h->getX() / map->grid_size, h->getY() / map->grid_size);
 }
 
 void Unit::attack(double attackRadius, double sectorAngle, double attackInterval) {
     //set attack interval of survivor
+     auto t_start = std::chrono::high_resolution_clock::now(); //timer begin no matter attack success or not
 
-     auto t_start = std::chrono::high_resolution_clock::now();
-    /*get all Handle that within radius (60 degree of the circle(radius = attack_radius, center = Coordinate(Survivor)) in front of Survivor)
+    /*get all Handle that within radius (60 degree of the circle(radius = attack_radius, center = Coordinate(Survivor)) in front of Survivor) 
+    This part is break down into two functions, one check handle in a given radius circle. And isInside sector function further check if the handles 
+    within the circule is inside the attack sector
+
     if :
         1. Handle is not invulnerable
         2. Handle is Unit
@@ -111,11 +115,10 @@ void Unit::attack(double attackRadius, double sectorAngle, double attackInterval
     }
     while (true)
     {
-    auto t_end = std::chrono::high_resolution_clock::now();
+    auto t_end = std::chrono::high_resolution_clock::now(); //get current time constantly until the required cooldown time is reached
     if (std::chrono::duration<float, milli>(t_end - t_start).count() > 1 / attackInterval)
     {
-        //cout << chrono::duration<float, milli>(t_end - t_start).count() << endl;
-        break;
+        break; //break the loop when time up and end the function
     }
     }
 }
--- Handle/Unit/Unit.h
+++ Handle/Unit/Unit.h
@@ -1,3 +1,5 @@
+//This is the base class for surviver and ghost, but also a dervied class of handle. It contains common functions like attack and
+//check handles within the circular sector. It also has visibility related functions. 
 #ifndef UNIT_H
 #define UNIT_H
 
@@ -6,23 +8,27 @@
 
 class Unit : public Handle {
     protected:
-        int damage;
-        float attackInterval;
-        vector<Effect*> EffectList;
+        int damage; //created as the inital variable for the functions to use. Survivor and ghost function will input their own value
+        float attackInterval; //put it here so that survivor and ghost can use it
+        vector<Effect*> EffectList; //vector contain of effect
         int visible_size; // base on grid && is a square for simplicity
-        explicit Unit(Map *map, double x, double y);
+        explicit Unit(Map *map, double x, double y); //constructor
+
     public:
-        float getDamage() const;
+        float getDamage() const; 
         void setDamage(int damage);
         virtual float getArmor() const;
-        virtual int getVisibleSize() const;
+        virtual int getVisibleSize() const; //different accessors
+
         virtual bool isGridVisible(int x, int y) const;
         virtual bool isHandleVisible(Handle *h) const;
-        void setVisibleSize(int visible_size);
-        void gainAttributeFromEffect(Effect *e);
+        void setVisibleSize(int visible_size); //visibility related funcitons
+
+        void gainAttributeFromEffect(Effect *e); //effect related functions
         void addEffect(Effect *e);
-        void attack(double attackRadius, double sectorAngle, double attackInterval);
-        bool isInsideSector(const Handle *h, double sector_angle) const;
+
+        void attack(double attackRadius, double sectorAngle, double attackInterval); //attack functon for player and ghost
+        bool isInsideSector(const Handle *h, double sector_angle) const; //a function to check if the position of the target is inside the circular sector
 
 };
 #endif // UNIT_H
