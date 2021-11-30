afc7c1cef4d338a3e6d593ea1ca33a1cbc23bb8d
qbgXyn <qbgXyn@gmail.com>
2021-11-30 21:30:00 +0800

    optimize

--- Map/Terrain.cpp
+++ Map/Terrain.cpp
@@ -1,6 +1,6 @@
 #include "Terrain.h"
 
-bool Terrain::operator()(Terrain::Type terrain) const { //count number of terrain, return true if there has at least one terrain 
+bool Terrain::operator()(Terrain::Type terrain) const { //count number of terrain, return true if it contains that type of terrain
     return t.count(terrain);
 }
 
